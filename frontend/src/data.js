70d9abb814c2443e6660ac0673e9672b8a6ea046
ishtiaq_ahmad <ishtiaqahmad427427@gmail.com>
2023-02-06 19:36:26 +0500

    Amazona rework

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+        _id: '1',
+        name: 'Shirt-1',
+        category: 'Cloths',
+        image: '/images/product-1.jpg',
+        price: 120,
+        countInStock: 10,
+        brand: 'Nike',
+        rating: 3.5,
+        numReviews: 10,
+        description: 'High Quality product'
+        },
+        {
+        _id: '2',
+        name: 'Shirt-2',
+        category: 'Cloths',
+        image: '/images/product-1.jpg',
+        price: 125,
+        countInStock: 8,
+        brand: 'Nike',
+        rating: 2.5,
+        numReviews: 15,
+        description: 'High Quality product'
+        },
+        {
+        _id: '3',
+        name: 'Shirt-3',
+        category: 'Cloths',
+        image: '/images/product-1.jpg',
+        price: 130,
+        countInStock: 6,
+        brand: 'Nike',
+        rating: 4.2,
+        numReviews: 10,
+        description: 'High Quality product'
+        },
+        {
+        _id: '4',
+        name: 'Shirt-4',
+        category: 'Cloths',
+        image: '/images/product-1.jpg',
+        price: 110,
+        countInStock: 19,
+        brand: 'Nike',
+        rating: 4.1,
+        numReviews: 10,
+        description: 'High Quality product'
+        },
+        {
+        _id: '5',
+        name: 'Shirt-5',
+        category: 'Cloths',
+        image: '/images/product-1.jpg',
+        price: 100,
+        countInStock: 15,
+        brand: 'Nike',
+        rating: 4.8,
+        numReviews: 5,
+        description: 'High Quality product'
+        },
+        {
+        _id: '6',
+        name: 'Shirt-6',
+        category: 'Cloths',
+        image: '/images/product-1.jpg',
+        price: 90,
+        countInStock: 0,
+        brand: 'Nike',
+        rating: 3.5,
+        numReviews: 10,
+        description: 'High Quality product'
+        },
+    ],
+}
+
+export default data;
